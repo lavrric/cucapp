8ea99e2519699d2f0959e2c281be7cd6fbfa2b5a
Mihail <mihai.lavric@deepcode.xyz>
2020-05-07 14:26:42 +0300

    added basic header

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.styles.scss";
+
+const Header = () => (
+  <div className="header">
+    <div className="title">CuCapp</div>
+    <div className="options">
+      <div className="option">Doneaza</div>
+      <div className="option">Echipa de colaboratori</div>
+    </div>
+  </div>
+);
+
+export default Header;
