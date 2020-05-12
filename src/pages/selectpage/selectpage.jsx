a9785190bee8c36a3d15dafdd4b75884669e73b0
Claudiu Babin <nibabin@Claudius-Mac-mini.local>
2020-05-12 11:34:42 +0300

    sds

--- src/pages/selectpage/selectpage.jsx
+++ src/pages/selectpage/selectpage.jsx
@@ -1,23 +1,12 @@
 import React, { Component } from 'react'
 import { firestore } from '../../firebase/firebase.utils'
-
+import GameList from '../../components/game-list/game-list'
 
 export default class SelectPage extends Component {
-    constructor() {
-        this.state = {
-            pachete: [],
-            searchBar: ''
-        }
-    }
-    
-    componentDidMount(){
-
-    }
-
     render() {
         return (
             <div>
-                
+                <GameList /> 
             </div>
         )
     }
