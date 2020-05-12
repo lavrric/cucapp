7d68caed848b087e80c5bab4f2ef6b8f7cbdd761
Claudiu Babin <nibabin@Claudius-Mac-mini.local>
2020-05-12 23:02:45 +0300

    added colors

--- src/components/game-item/game-item.jsx
+++ src/components/game-item/game-item.jsx
@@ -12,20 +12,29 @@ const GameItem = ({
   handleSelect,
   id,
   nivel,
-}) => (
-  <div
-    className={`${grey ? "grey" : ""} ${first ? "first" : ""} ${
-      selected.sezonul === sezonul && selected.etapa === etapa ? "selected" : ""
-    } game-item`}
-    onClick={(e) => handleSelect({ sezonul, etapa })}
-  >
-    <div className="game-item-characteristic id">{id}</div>
-    <div className="game-item-characteristic sezonul">{sezonul}</div>
-    <div className="game-item-characteristic etapa">{etapa}</div>
-    <div className="game-item-characteristic autori">{autori.join(", ")}</div>
-    <div className="game-item-characteristic nivel">{nivel}</div>
-    <div className="game-item-characteristic nr_intrebari">{nr_intrebari}</div>
-  </div>
-);
+}) => {
+    let color = '';
+    switch(nivel){
+      case 'Mediu': color = 'rgb(255, 204, 0)'; break;
+      case 'Dificil': color = 'rgb(255, 102, 102)'; break;
+      case 'Simplu': color = 'rgb(102, 255, 51)'; break;
+      default: color = 'rgb(0,0,0)'
+    }
+    return (
+    <div
+      className={`${grey ? "grey" : ""} ${first ? "first" : ""} ${
+        selected.sezonul === sezonul && selected.etapa === etapa ? "selected" : ""
+      } game-item`}
+      onClick={(e) => handleSelect({ sezonul, etapa })}
+    >
+      <div className="game-item-characteristic id">{id}</div>
+      <div className="game-item-characteristic sezonul">{sezonul}</div>
+      <div className="game-item-characteristic etapa">{etapa}</div>
+      <div className="game-item-characteristic autori">{autori.join(", ")}</div>
+      <div className="game-item-characteristic nivel" style={{color: color, 'font-weight': 'bold'}}>{nivel}</div>
+      <div className="game-item-characteristic nr_intrebari">{nr_intrebari}</div>
+    </div>
+    )  
+  };
 
 export default GameItem;
