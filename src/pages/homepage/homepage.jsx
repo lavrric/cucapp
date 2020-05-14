bed4e6a8016d19df4b665c04fd2a0957812db016
Babin Claudiu <babinclaudiu@gmail.com>
2020-05-14 22:38:30 +0300

    added svg

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -12,10 +12,12 @@ const HomePage = () => (
         title="Joacă Pachete"
         text="Joacă pachete Ce Unde Când din arhiva Clubului de Jocuri Intelectuale din Moldova. La moment nu sunt disponibile atât de multe pachete câte ne-am dori, dar pe parcurs vom adăuga cât mai multe (în ordine cronologică)."
       />
+      <div className="bar"></div>
       <Feature
         title="Joacă Întrebări"
         text="Joacă întrebări alese random, dar cu cap de sistemul din spate. Alegerea perfectă în cazul în care dorești să ai parte de un joc divers cu întrebări noi, dar și vechi, de la autori diferiți, și în stiluri cât mai variate."
       />
+      <div className="bar"></div>
       <Feature
         title="Caută Întrebări"
         text="Mick-unealta perfectă! Acestă mick-unealtă vă va permite să gasiți intrebarea aia care vă stă de atâta timp pe limba dar nu v-o puteți aminti. Perfect într-adevăr! Planificăm să adăugam acest feature cât de curând posibil. "
