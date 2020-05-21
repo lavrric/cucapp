2d9608bd7684b4aa125470ba78ef9cca53fd63ed
Mihail <mlavric64@gmail.com>
2020-05-21 18:43:28 +0300

    finished endpage, looking good rn

--- src/pages/endpage/endpage.jsx
+++ src/pages/endpage/endpage.jsx
@@ -1,136 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import "./endpage.scss";
 
-const review = [
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: true,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga ",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-  {
-    intrebare:
-      "Aici e o intrebare pizdet ce lunga lunga lunga lunga lunga lunga lunga lunga lunga lunga",
-    raspuns: "Aici e un raspuns pizdet pizdet ce lung lung lung",
-    raspuns_user: "Aici e rapsunsul de user",
-    isCorrect: false,
-  },
-];
-
-const Endpage = () => (
-  <div className="endpage">
-    <div className="intro">
-      <div className="title">Cuc Review</div>
-      <div className="title">
-        Scorul tău: <span style={{ color: "#49ad40" }}>19</span>:
-        <span style={{ color: "rgb(255, 102, 102)" }}>21</span>
+function Endpage({ intrebari, answerUser, judge }) {
+  const [selected, selectNew] = useState(-1);
+  return (
+    <div className="endpage">
+      <div className="intro">
+        <div className="title">Cuc Review</div>
+        <div className="title">
+          Scorul tău:{" "}
+          <span style={{ color: "#49ad40" }}>{`${judge.reduce(
+            (cur, value) => cur + (value ? 1 : 0),
+            0
+          )}`}</span>
+          :
+          <span style={{ color: "rgb(255, 102, 102)" }}>{`${judge.reduce(
+            (cur, value) => cur + (value ? 0 : 1),
+            0
+          )}`}</span>
+        </div>
+      </div>
+      <div className="review">
+        <div className="review-item" style={{ cursor: "context-menu " }}>
+          <div className="option id">#</div>
+          <div className="option intrebare">
+            Întrebarea (click pentru întrebarea completă)
+          </div>
+          <div className="option raspuns">Răspunsul corect</div>
+          <div className="option raspuns_user">Răspunsul oferit</div>
+          <div className="option corect">Corectitudinea</div>
+        </div>
+        {intrebari.map(({ intrebare, raspuns }, i) => (
+          <div
+            className={`review-item ${selected === i ? "selected" : ""} ${
+              i % 2 === 0 ? "grey" : ""
+            }`}
+            onClick={() => selectNew(i === selected ? -1 : i)}
+          >
+            <div className="option id">{i + 1}</div>
+            <div className="option intrebare">
+              {i === selected || intrebare.length <= 69
+                ? intrebare
+                : `${intrebare.substr(0, 69)}...`}
+            </div>
+            <div className="option raspuns">
+              {i === selected ||
+              raspuns.split(
+                /Comentari[ui]:|Surs[aă]:|Comentariu|Surs[aă]|S:|C:/
+              )[0].length <= 20
+                ? raspuns.split(
+                    /Comentari[ui]:|Surs[aă]:|Comentariu|Surs[aă]|S:|C:/
+                  )[0]
+                : `${raspuns
+                    .split(
+                      /Comentari[ui]:|Surs[aă]:|Comentariu|Surs[aă]|S:|C:/
+                    )[0]
+                    .substr(0, 20)}...`}
+            </div>
+            <div className="option raspuns_user">
+              {i === selected || answerUser[i].length <= 20
+                ? answerUser[i]
+                : `${answerUser[i].substr(0, 20)}...`}
+            </div>
+            <div
+              className="option corect"
+              style={{
+                fontWeight: "bold",
+                color: judge[i] ? "#49ad40" : "rgb(255, 102, 102)",
+              }}
+            >
+              {judge[i] ? "Corect" : "Greșit"}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-    <div className="review">
-      {review.map(({ intrebare, raspuns, raspuns_user, isCorrect }, i) => (
-        <div className="review-item">
-          <div className="option id">{i}</div>
-          <div className="option intrebare">{intrebare}</div>
-          <div className="option raspuns">{raspuns}</div>
-          <div className="option raspuns_user">{raspuns_user}</div>
-          <div
-            className="option corect"
-            style={{
-              fontWeight: "bold",
-              color: isCorrect ? "#49ad40" : "rgb(255, 102, 102)",
-            }}
-          >
-            {isCorrect ? "Corect" : "Greșit"}
-          </div>
-        </div>
-      ))}
-    </div>
-  </div>
-);
+  );
+}
 
 export default Endpage;
