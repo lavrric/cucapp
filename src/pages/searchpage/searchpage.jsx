d32e93e8b1e4529b967d81c173f51776d86ca633
Babin Claudiu <babinclaudiu@gmail.com>
2020-05-21 18:44:57 +0300

    searchpage

--- src/pages/searchpage/searchpage.jsx
+++ src/pages/searchpage/searchpage.jsx
@@ -1,18 +1,42 @@
 import React from "react";
 import algoliasearch from "algoliasearch/lite";
-import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
-import './searchpage.scss'
-
+import { InstantSearch, SearchBox, Hits, Highlight } from "react-instantsearch-dom";
+import "./searchpage.scss";
 
 const searchClient = algoliasearch(
   "W5FIV8V91S",
   "ba881b9e7c90e3c9d48d9a0bb2c9a629"
 );
 
+const Hit = ({ hit }) => (
+  <>
+    <b>Întrebare: </b>
+    <Highlight attribute="intrebare" hit={hit} />
+    <br />
+    <b>Răspuns: </b>
+    <Highlight attribute="raspuns" hit={hit} />
+    {hit.autor.length ? (
+      <>
+        <br />
+        <b>Autor: </b>
+        <Highlight attribute="autor" hit={hit} />
+      </>
+    ) : (
+      <></>
+    )}
+    <br />
+    <br />
+    {hit.objectID.split("Camp ")[1]}
+  </>
+);
+
 const SearchPage = () => (
   <InstantSearch searchClient={searchClient} indexName="intrebari_search">
-    <SearchBox className="searchbar" translations={{placeholder: 'Caută după întrebare/autor/răspuns'}}/>
-    <Hits />
+    <SearchBox
+      className="searchbar"
+      translations={{ placeholder: "Caută după întrebare/autor/răspuns" }}
+    />
+    <Hits hitComponent={Hit} />
   </InstantSearch>
 );
 
